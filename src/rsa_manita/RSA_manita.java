b0ce1d5f4df26658098f241b63b4b407511729ba
BarreraOlmos <cbarrerao2100@alumno.ipn.mx>
2023-11-21 23:21:24 -0600

    Commit final (espero)

---
+++ src/rsa_manita/RSA_manita.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package rsa_manita;
+
+import INTERFACES_GUI.Cifrar_texto;
+
+/**
+ *
+ * @author jerak_ba
+ */
+public class RSA_manita {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Cifrar_texto main = new Cifrar_texto();
+        main.setVisible(true);
+        main.setLocationRelativeTo(null);
+
+    }
+    
+}
